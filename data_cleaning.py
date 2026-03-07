bacf963bda1cdaa9768e9bc26d1b9234e43ffb7b
ammar prasla <apa105@sfu.ca>
2026-03-07 00:49:06 -0800

    removed outliers

--- data_cleaning.py
+++ data_cleaning.py
@@ -11,4 +11,14 @@ drop_cols = [
 
 bc_df = bc_df.drop(columns=drop_cols, errors="ignore")
 
+bc_df = bc_df[(bc_df["Price"] >= 100000) & (bc_df["Price"] <= 5000000)]
+
+bc_df = bc_df[(bc_df["Square Footage"] >= 200) & (bc_df["Square Footage"] <= 10000)]
+
+bc_df = bc_df[(bc_df["Bedrooms"] >= 1) & (bc_df["Bedrooms"] <= 10)]
+bc_df = bc_df[(bc_df["Bathrooms"] >= 1) & (bc_df["Bathrooms"] <= 10)]
+
+print("Rows after cleaning:", len(bc_df))
+
 bc_df.to_csv("cleaned_bc_data.csv", index=False)
+
